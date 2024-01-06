4b6169336a4d518ec7da6867cc99574ca8a58ebd
VIET <viet123549@gmail.com>
2024-01-06 09:22:31 +0700

    fix hanlde error

--- app.js
+++ app.js
@@ -29,6 +29,12 @@ app.use((req, res, next) => {
     next(error)
 })
 
-app.use((error, req, res, next) => res.status(error.status || 500).json({ error: error.message }))
+app.use((error, req, res, next) => {
+    const statusCode = error.statusCode || 500
+    res.status(statusCode).json({ 
+        code: statusCode,
+        message: error.message
+     })
+})
 
 module.exports = app
